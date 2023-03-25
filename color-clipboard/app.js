6217351dbde82f3b445c06edb0aab558dad8ee12
findingsimo <djsimo3001@gmail.com>
2023-03-25 15:04:35 +0100

    Color Clipboard
    New app

---
+++ color-clipboard/app.js
@@ -0,0 +1,54 @@
+const colorList = document.getElementById("colorList");
+let colorValues = [];
+
+function isHexColor(hex) {
+    return /^#([0-9A-F]{3}){1,2}$/i.test(hex);
+}
+
+async function requestClipboardPermission() {
+    try {
+        const { state } = await navigator.permissions.query({ name: "clipboard-read" });
+        if (state === "granted" || state === "prompt") {
+            return true;
+        }
+    } catch (err) {
+        console.error('Failed to request clipboard permission:', err);
+    }
+    return false;
+}
+
+async function readClipboard() {
+    try {
+        const hasPermission = await requestClipboardPermission();
+        if (!hasPermission) {
+            console.error('Clipboard permission not granted');
+            return;
+        }
+        const text = await navigator.clipboard.readText();
+        if (isHexColor(text) && colorValues.length < 5) {
+            colorValues.push(text);
+            updateColorList();
+        }
+    } catch (err) {
+        console.error('Failed to read clipboard:', err);
+    }
+}
+
+function updateColorList() {
+    colorList.innerHTML = "";
+    colorValues.forEach(color => {
+        const li = document.createElement("li");
+        const colorPreview = document.createElement("div");
+        colorPreview.classList.add("color-preview");
+        colorPreview.style.backgroundColor = color;
+        li.appendChild(colorPreview);
+        li.appendChild(document.createTextNode(color));
+        colorList.appendChild(li);
+    });
+}
+
+document.addEventListener("visibilitychange", () => {
+    if (!document.hidden) {
+        readClipboard();
+    }
+});
